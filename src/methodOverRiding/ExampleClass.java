dafd790fbc1821acd2ecca5e26233e13ccf59971
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-05-09 16:37:08 +0300

    methodOverRiding paketi oluşturuldu

---
+++ src/methodOverRiding/ExampleClass.java
@@ -0,0 +1,9 @@
+package methodOverRiding;
+
+public abstract class ExampleClass {
+
+    public abstract void giris();
+    public abstract void giris(String str);
+
+    public abstract void giris(String name, int age);
+}
