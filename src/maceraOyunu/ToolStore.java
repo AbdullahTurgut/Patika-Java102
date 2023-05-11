024ca3037f84edb92768bcd177254766a4f0dfa9
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-05-11 15:45:04 +0300

    Weapon sınıfı oluşturuldu.

--- src/maceraOyunu/ToolStore.java
+++ src/maceraOyunu/ToolStore.java
@@ -14,12 +14,12 @@ public class ToolStore extends NormalLoc {
         System.out.print("Seçiminiz: ");
         int selectCase = Location.scanner.nextInt(); // static->artık her nesne üretildiğinde üretilmeyecek
 
-        while (selectCase < 1 || selectCase > 3){
+        while (selectCase < 1 || selectCase > 3) {
             System.out.print("Geçersiz değer , tekrar giriniz: ");
             selectCase = scanner.nextInt(); // ata sınıftan geldiği için Location. yazmamıza gerek kalmaz
         }
 
-        switch (selectCase){
+        switch (selectCase) {
             case 1:
                 printWeapons();
                 break;
@@ -34,12 +34,34 @@ public class ToolStore extends NormalLoc {
         return true;
     }
 
-    public void printWeapons(){
+    public void printWeapons() {
         System.out.println("---Silahlar---");
+        for (Weapon weapon : Weapon.weapons()) { // static olduğu için nesne üretmeden silah listesini aldık
+            System.out.println(weapon.getId() + "- " + weapon.getName()
+                    + " <Para: " + weapon.getPrice()
+                    + "  Hasar: " + weapon.getDamage() + ">");
+        }
+        System.out.print("Bir silah seçiniz: ");
+        int selectWeaponID = scanner.nextInt();
+        while (selectWeaponID < 1 || selectWeaponID > Weapon.weapons().length) {
+            System.out.print("Geçersiz değer , tekrar giriniz: ");
+            selectWeaponID = scanner.nextInt();
+        }
+        Weapon selectedWeapon = Weapon.getWeaponById(selectWeaponID);
 
+        if (selectedWeapon != null) {
+            if (selectedWeapon.getPrice() > this.getPlayer().getMoney()) {
+                System.out.println("Yeterli paranız bulunmamaktadır !");
+            } else {
+                System.out.println(selectedWeapon.getName() + " silahını satın aldınız !");
+                int balance = this.getPlayer().getMoney() - selectedWeapon.getPrice();
+                this.getPlayer().setMoney(balance);
+                System.out.println("Kalan paranız: " + this.getPlayer().getMoney());
+            }
+        }
     }
 
-    public void printArmors(){
+    public void printArmors() {
         System.out.println("---Zırhlar---");
     }
 }
