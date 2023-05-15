b7b26b08eb9dfe74581c0bd1d5b6cb115d598cca
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-05-15 03:20:11 +0300

    generics paketi içerisinde Print sınıfı oluşturuldu

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -2,8 +2,15 @@ package generics;
 
 public class Main {
     public static void main(String[] args) {
-        GenericNullable<Integer> n = new GenericNullable<>(10);
-        n.run();
+        String[] a = {"Java", "102", "Patika", "Dev"};
+        Integer[] b = {1, 2, 3, 4};
+        Character[] c = {'J', 'A', 'V', 'A'};
+
+        Print.printArray(a);
+        Print.printArray(b);
+
+//        GenericNullable<Integer> n = new GenericNullable<>(10);
+//        n.run();
 
 //        Integer a = null;
 //        IntegerNullable integerNullable = new IntegerNullable(a);
---
+++ src/generics/Print.java
@@ -0,0 +1,10 @@
+package generics;
+
+public class Print {
+    //generic method
+    public static <T> void printArray(T[] arr){
+        for(T i : arr){
+            System.out.println(i);
+        }
+    }
+}
