8314a7b078521bfff6c361cb4b99fb1080778aba
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-05-16 21:13:46 +0300

    setInterfaceAndHashSet paketi içerisinde treeSetExamples paketi oluşturuldu

---
+++ src/collections/setInterfaceAndHashSet/treeSetExamples/Main.java
@@ -0,0 +1,24 @@
+package collections.setInterfaceAndHashSet.treeSetExamples;
+
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // bu şekilde hata alınır. Sıralama değeri vermek gerekiyor
+//        TreeSet<Student> students = new TreeSet<>(new OrderNoteComparator()); // küçükten büyüğe sırala
+//        TreeSet<Student> students = new TreeSet<>(new OrderNoteComparator().reversed()); // büyükten küçüğe sırala
+        TreeSet<Student> students = new TreeSet<>(new OrderNameComparator());
+
+        students.add(new Student("Abdullah", 20));
+        students.add(new Student("Asya", 90));
+        students.add(new Student("Eray", 70));
+        students.add(new Student("Kübra", 95));
+        students.add(new Student("Zehra", 80));
+
+        for (Student student : students)
+            System.out.println(student.getName());
+
+
+    }
+}
---
+++ src/collections/setInterfaceAndHashSet/treeSetExamples/OrderNoteComparator.java
@@ -0,0 +1,13 @@
+package collections.setInterfaceAndHashSet.treeSetExamples;
+
+import java.util.Comparator;
+
+public class OrderNoteComparator implements Comparator<Student> {
+
+    // sıralama yapabilmek için negatif deger gelmesi durumunda
+    // o2 ye göre sıralama yapacaktır
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.getNote() - o2.getNote();
+    }
+}
