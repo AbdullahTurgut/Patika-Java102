8e98258056d3d5a29f60bb08cb4fa1976db37746
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-05-17 22:19:43 +0300

    linkedHashSetExamples paketi oluşturuldu ve linkedHashSet sınıfına dair örnekler kodlandı.

---
+++ src/collections/VectorExamples.java
@@ -0,0 +1,7 @@
+package collections;
+
+public class VectorExamples {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/collections/setInterfaceAndHashSet/linkedHashSetExamples/Main.java
@@ -0,0 +1,21 @@
+package collections.setInterfaceAndHashSet.linkedHashSetExamples;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
+
+        linkedHashSet.add(20);
+        linkedHashSet.add(10);
+        linkedHashSet.add(10);
+        linkedHashSet.add(30);
+
+        Iterator<Integer> itr = linkedHashSet.iterator();
+
+        while (itr.hasNext()){
+            System.out.println(itr.next()); // girilen sıra ile verir değeri ve tekrarlanmaz
+        }
+    }
+}
