cd9ff2addab2d884176e3f877f7e3188be2f4ba6
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-05-25 01:46:12 +0300

    readersClasses paketi içerisinde InputStreamReader örneği oluşturuldu.

---
+++ src/io/readersClasses/Main.java
@@ -0,0 +1,28 @@
+package io.readersClasses;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/io/readersClasses/patika.txt");//okuma işlemi
+
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream); // tr karakter olayını düzeltmek için
+            System.out.println(inputStreamReader.getEncoding()); // çıktı -> UTF8
+            int i = inputStreamReader.read();
+            while (i != -1){
+                System.out.print((char)i);
+                i = inputStreamReader.read(); // bu şekilde tr karakterleride okuyabiliriz
+            }
+//            int i = fileInputStream.read();
+//            while(i != -1){
+//                System.out.print((char) i); // çıktıda tr karakterler sorun yapar
+//                i = fileInputStream.read();
+//            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
